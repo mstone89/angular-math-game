f3c6bfa1d6f22dd964ca78f92d5e6121778e5890
Molly Stone <molly.stone89@gmail.com>
2020-02-26 19:54:26 -0500

    Create custom directive for adding class to answers

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Directive({
-  selector: '[appAnswerHighlight]'
+    selector: '[appAnswerHighlight]'
 })
-export class AnswerHighlightDirective {
+export class AnswerHighlightDirective implements OnInit {
 
-  constructor() { }
+    constructor(private element: ElementRef, private controlName: NgControl) { }
 
+    ngOnInit() {
+        this.controlName.control.parent.valueChanges
+            .pipe(
+                map(({valueOne, valueTwo, answer}) => {
+                    return Math.abs(valueOne + valueTwo - answer) / (valueOne + valueTwo);
+                })
+            ).subscribe((value) => {
+                if (value < 0.2) {
+                    this.element.nativeElement.classList.add('close');
+                } else {
+                    this.element.nativeElement.classList.remove('close');
+                }
+            });
+    }
 }
